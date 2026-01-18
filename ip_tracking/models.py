d6a038f4db308290060e835e32c5a919fabb5217
alaametwale <132303984+alaametwale@users.noreply.github.com>
2026-01-18 16:30:48 +0200

    Create models.py

---
+++ ip_tracking/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class RequestLog(models.Model):
+    ip_address = models.GenericIPAddressField()
+    path = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.path}"
+
+
+class BlockedIP(models.Model):
+    ip_address = models.GenericIPAddressField(unique=True)
+
+    def __str__(self):
+        return self.ip_address
+
+
+class SuspiciousIP(models.Model):
+    ip_address = models.GenericIPAddressField()
+    reason = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.ip_address} - {self.reason}"
